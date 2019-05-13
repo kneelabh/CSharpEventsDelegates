884d817f20300dd8bb06745f448feeaa7072d131
Neelabh <kneelabh@omnicomhealthgroup.com>
2019-05-13 19:05:07 +0530

    Adding code

---
+++ ImageProcessor.cs
@@ -0,0 +1,15 @@
+﻿namespace Events_Delegates
+{
+	public delegate void ApplyImageFilters(object sender, MessageArgs args);
+	internal class ImageProcessor
+	{
+		public int Counter { get; internal set; }
+
+		public event ApplyImageFilters OnFilter;
+
+		internal void Process()
+		{
+			OnFilter(this, new MessageArgs() { source = "Filter Applied: " });
+		}
+	}
+}
